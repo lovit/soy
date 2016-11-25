8b060bc49899580707c60d295b0c4f66cdca06cd
Hyunjoong Kim <lovit@users.noreply.github.com>
2016-11-25 17:03:01 +0900

    Update utils.py

--- soy/utils.py
+++ soy/utils.py
@@ -94,10 +94,11 @@ class IntegerEncoder:
         
 class Corpus:
     
-    def __init__(self, corpus_fname, num_doc = -1, num_sent = -1):
+    def __init__(self, corpus_fname, num_doc = -1, num_sent = -1, iter_sent = False):
         self.corpus_fname = corpus_fname
         self.num_doc = num_doc
         self.num_sent = num_sent
+        self.iter_sent = iter_sent
 
         if num_doc <= 0:
             with open(corpus_fname, encoding='utf-8') as f:
@@ -111,26 +112,26 @@ class Corpus:
                 self.num_doc += 1
                 if num_sent <= 0:
                     self.num_sent = num_sent_tmp
-        
-        
-    def __iter__(self):
-        with open(self.corpus_fname, encoding='utf-8') as f:
-            for doc_id, doc in enumerate(f):
-                if doc_id >= self.num_doc:
-                    break
-                yield doc.strip()
-
+    
                 
-    def iter_sents(self):
+    def __iter__(self):
+        
         with open(self.corpus_fname, encoding='utf-8') as f:
+            
             for doc_id, doc in enumerate(f):
+                
                 if doc_id >= self.num_doc:
                     break
-                for sent in doc.split('  '):
-                    sent = sent.strip()
-                    if not sent:
-                        continue
-                    yield sent.strip()
+                    
+                if not self.iter_sent:
+                    yield doc
+                    
+                else:
+                    for sent in doc.split('  '):
+                        sent = sent.strip()
+                        if not sent:
+                            continue
+                        yield sent.strip()
     
     
     def __len__(self):
