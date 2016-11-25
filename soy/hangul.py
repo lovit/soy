31fb1cb3728acfc8e307ab79f2801dbf125a5da9
Hyunjoong Kim <lovit@users.noreply.github.com>
2016-11-25 17:13:12 +0900

    Update hangul.py

--- soy/hangul.py
+++ soy/hangul.py
@@ -1,17 +1,17 @@
-kor_begin     = 44032
-kor_end       = 55199
-chosung_base  = 588
-jungsung_base = 28
+_kor_begin     = 44032
+_kor_end       = 55199
+_chosung_base  = 588
+_jungsung_base = 28
 
-chosung_list = [ 'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 
+_chosung_list = [ 'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 
         'ㅅ', 'ㅆ', 'ㅇ' , 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
 
-jungsung_list = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 
+_jungsung_list = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 
         'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 
         'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 
         'ㅡ', 'ㅢ', 'ㅣ']
 
-jongsung_list = [
+_jongsung_list = [
     ' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ',
         'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 
         'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 
@@ -24,12 +24,12 @@ def normalize(doc, english=False, number=False, punctuation=False, remains={}):
     
     for c in doc:
 
-        i = ord(doc)
+        i = ord(c)
         
         if c == ' ':
             f += ' '
         
-        elif (i >= kor_begin) and (i <= kor_end):
+        elif (i >= _kor_begin) and (i <= _kor_end):
             f += c
             
         elif (english) and ( (i >= 97 and i <= 122) or (i >= 65 and i <= 90) ):
@@ -51,17 +51,17 @@ def split_jamo(c):
     
     base = ord(c)
     
-    if base < kor_begin or base > kor_end:
+    if base < _kor_begin or base > _kor_end:
         return None
     
-    base -= kor_begin
+    base -= _kor_begin
     
-    cho  = base // chosung_base
-    jung = ( base - cho * chosung_base ) // jungsung_base 
-    jong = ( base - cho * chosung_base - jung * jungsung_base )
+    cho  = base // _chosung_base
+    jung = ( base - cho * _chosung_base ) // _jungsung_base 
+    jong = ( base - cho * _chosung_base - jung * _jungsung_base )
     
-    return [chosung_list[cho], jungsung_list[jung], jongsung_list[jong]]
+    return [_chosung_list[cho], _jungsung_list[jung], _jongsung_list[jong]]
 
 
 def combine_jamo(chosung, jungsung, jongsung):
-    return chr(kor_begin + chosung_base * chosung_list.index(chosung) + jungsung_base * jungsung_list.index(jungsung) + jongsung_list.index(jongsung))
+    return chr(_kor_begin + _chosung_base * _chosung_list.index(chosung) + _jungsung_base * _jungsung_list.index(jungsung) + _jongsung_list.index(jongsung))
