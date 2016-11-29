a5e2c4ed5b5e19801a6758660637d65eeafbb91d
lovit <soy.lovit@gmail.com>
2016-11-30 00:32:07 +0900

    Move soy/[hangle, tags] to soy/nlp/[hangle, tags]

--- soy/__init__.py
+++ soy/__init__.py
@@ -1,6 +1,5 @@
 __author__ = 'lovit'
 __version__ = '0.0.1'
 
-from . import hangle
+from . import nlp
 from . import utils
-from . import tags
---
+++ soy/nlp/__init__.py
@@ -0,0 +1,2 @@
+from . import tags
+from . import hangle
