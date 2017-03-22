2c4dfa6d2557f67ad404cc88483ae49db5e57487
lovit <soy.lovit@gmail.com>
2017-03-22 17:04:18 +0900

    Add import in __init__

--- soy/nlp/__init__.py
+++ soy/nlp/__init__.py
@@ -3,3 +3,4 @@ from . import hangle
 from . import tags
 from . import space
 from . import tokenizer
+from . import representations
---
+++ soy/nlp/representations/__init__.py
@@ -0,0 +1,1 @@
+from ._bag_of_concepts import ConceptMapperBuilder
