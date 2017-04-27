d30e7717b938d4230c9ffe3508e0376bdd929d36
lovit <soy.lovit@gmail.com>
2017-04-27 15:06:07 +0900

    Add import

--- soy/utils/__init__.py
+++ soy/utils/__init__.py
@@ -3,4 +3,6 @@ from ._gensim import DoublespaceLineDoc2VecCorpus
 from ._gensim import DoublespaceLineWord2VecCorpus
 from ._utils import IntegerEncoder
 from ._utils import daterange
+from ._utils import get_available_memory
+from ._utils import get_process_memory
 from ._utils import progress
